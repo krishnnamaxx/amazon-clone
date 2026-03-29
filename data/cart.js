668ddbc4a90faa00c2fc80f1602c9c8ab297c6e1
Krishna <krishna9842740944@gmail.com>
2026-03-29 22:02:00 +0530

    Updated the delivery date in checkout page

--- data/cart.js
+++ data/cart.js
@@ -3,14 +3,15 @@
   if(!cart){
  cart=[{
     productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-    quantity:2
+    quantity:2,
+    deliveryOptionId :'1'
   },{
     productId :"3ebe75dc-64d2-4137-8860-1f5a963e534b",
-    quantity:1
+    quantity:1,
+    deliveryOptionId :'2'
   }];
   }
   
- 
 
   function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart));
@@ -34,7 +35,8 @@
           else {
               cart.push({
               productId,
-              quantity
+              quantity,
+              deliveryOptionId :'1'
           });
           }
           saveToStorage();
---
+++ data/deliveryOptions.js
@@ -0,0 +1,16 @@
+ export const deliveryOptions=[
+    {
+        id : '1',
+        deliveryDate :7,
+        price : 0
+    },{
+        id :'2',
+        deliveryDate :3,
+        price : 99
+
+    },{
+        id :'3',
+        deliveryDate : 1,
+        price : 199
+    }
+];
